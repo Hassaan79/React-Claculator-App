9428fcbc29b3f4815c8dbb47183f874f8b5afeed
Hassaan79 <130352083+Hassaan79@users.noreply.github.com>
2025-02-17 23:36:56 +0500

    changes

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// src/App.js
+
+import React from 'react';
+import Calculator from './Components/Calculator';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
